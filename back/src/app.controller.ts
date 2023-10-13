a1024bdd46d43c87adaf3cea50036de3e05820e8
Mathis <mathislag@hotmail.fr>
2023-10-13 09:21:38 +0200

    ajout git ignore + db

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,23 +1,16 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/auth.guard';
 
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
-
-@Controller('auth')
-export class AuthController {
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
+  @UseGuards(JwtAuthGuard)
+  @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
+
--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
-
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [AuthModule,
+  imports: [
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'test',
-    password: 'test',
-    database: 'test',
-    entities: [User],
-    synchronize: true,
-  }),
-  TypeOrmModule.forFeature([User]),],
-  controllers: [AppController],
-  providers: [AppService],
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'db',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    JwtModule,
+  ],
+  providers: [UsersService, AuthService, JwtAuthGuard],
 })
 export class AppModule {}
