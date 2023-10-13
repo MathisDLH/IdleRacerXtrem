4e4dc77cb1e442c387b642cb9fdb58209c69fd4c
Mathis <mathislag@hotmail.fr>
2023-10-13 13:58:24 +0200

    ,

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('Doc IdleRacer Api')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
