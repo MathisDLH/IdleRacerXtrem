a532325d241880d147fc1d24b14f1838b2804121
Sébastien Candau <sebastiencandau>
2023-10-19 16:27:37 +0200

    user context + api connexion + cors

--- front/src/views/Home.tsx
+++ front/src/views/Home.tsx
@@ -2,23 +2,48 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/Home.scss'; // Importez votre fichier CSS ici
 import backgroundImage from '../assets/images/idle_racer_bg.png'; // Chemin vers l'image
 import flags from '../assets/images/race_flag.png';
-import {motion} from 'framer-motion';
+import carIcon from '../assets/images/auth/car_icon.png';
+import { motion } from 'framer-motion';
+import { useAuth } from '../context/Auth';
+import { useEffect, useState } from 'react';
+import LoginForm from './LoginForm';
+import { Modal } from '@mui/material';
+import jwtDecode from 'jwt-decode';
+import { getUser } from '../services/auth.service';
 
 
 const Home = () => {
-  return (
-    <motion.div className="menu" style={{ backgroundImage: `url(${backgroundImage})` }}
 
-    initial={{ opacity: 0, scale: 0, rotate: 45 }}
-    animate={{ opacity: 1, scale: 1, rotate: 0 }}
-    exit={{ opacity: 0, scale: 0, rotate: 45 }}
+  const { user, isLoggedIn } = useAuth();
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const closeModal = () => {
+    setModalVisible(false);
+  }
+
+  return (
+    <motion.div style={{ backgroundImage: `url(${backgroundImage})` }}
+
+      initial={{ opacity: 0, scale: 0, rotate: 45 }}
+      animate={{ opacity: 1, scale: 1, rotate: 0 }}
+      exit={{ opacity: 0, scale: 0, rotate: 45 }}
     >
-      <div className="menu-content">
-        <img src={flags} alt="race_flag" className="image-above-title" />
-        <h1 className="game-title">Idle Racer</h1>
-        <Link to="/game">
-          <button className="start-button">START</button>
-        </Link>
+      <Modal open={modalVisible} className='login-modal'>
+        <LoginForm closeModal={closeModal} />
+      </Modal>
+      <div className='head-container' onClick={() => { if (!isLoggedIn) { setModalVisible(true) } }}>
+        <img alt='car_icon' src={carIcon} className='car-icon' />
+        <p>{isLoggedIn && user ? user.email : "Login"}</p>
+      </div>
+      <div className="menu">
+        <div className="menu-content">
+          <img src={flags} alt="race_flag" className="image-above-title" />
+          <h1 className="game-title">Idle Racer</h1>
+          <Link to="/game">
+            <button className={user ? "start-button" : "start-button-desactived"}>START</button>
+          </Link>
+        </div>
       </div>
     </motion.div>
   );
---
+++ front/src/views/RegisterForm.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useAuth } from '../context/Auth';
+import { Link, useNavigate } from "react-router-dom";
+
+import backgroundImage from '../assets/images/idle_racer_bg.png'; // Chemin vers l'image
+import flags from '../assets/images/race_flag.png';
+import { motion } from 'framer-motion';
+
+const RegisterForm = () => {
+  const { register } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const userData = await register(email, password);
+      navigate('/');
+      console.log('Utilisateur inscrit avec succès:', userData);
+    } catch (error) {
+      console.error(`Erreur lors de l'inscription`, error);
+    }
+  };
+
+  return (
+    <motion.div
+      className="menu"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+      initial={{ opacity: 0, scale: 0, rotate: 45 }}
+      animate={{ opacity: 1, scale: 1, rotate: 0 }}
+      exit={{ opacity: 0, scale: 0, rotate: 45 }}
+    >
+      <div className="menu-content">
+        <img src={flags} alt="race_flag" className="image-above-title" />
+        <h1 className="game-title">Idle Racer</h1>
+        <h1 className="page-title">Inscription</h1>
+        <form onSubmit={handleSignup}>
+          <div className="form-group">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-input"
+            />
+          </div>
+          <button type="submit" className="start-button">
+            S'inscrire
+          </button>
+        </form>
+        <Link to='/' style={{cursor: 'pointer', color: 'black', textDecoration: 'underline'}}><p>EXIT</p></Link>
+      </div>
+    </motion.div>
+  );
+}
+
+export default RegisterForm;
