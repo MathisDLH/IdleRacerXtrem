003c91fd219421fd418b7bc914aa6e75bc92ab23
augcolas <augus.colas@orange.fr>
2023-10-20 08:34:12 +0200

    feat !: updating upgrade component

--- front/src/components/Upgrade.tsx
+++ front/src/components/Upgrade.tsx
@@ -1,11 +1,17 @@
 import '../assets/styles/Upgrade.scss';
 import {UpgradeModel} from '../models/Upgrade.tsx';
+import GameButton from "./GameButton.tsx";
 
 export default function Upgrade(upgrade: UpgradeModel) {
 
+    function click(){
+
+    }
+
     return(
-        <div className={"upgrade"}>
-            {upgrade.name}
+        <div className={"upgrade prevent-select"}>
+            <span>{upgrade.name}</span>
+            <GameButton color={"green"} text={upgrade.price + "$"} click={click}></GameButton>
         </div>
     )
 }
