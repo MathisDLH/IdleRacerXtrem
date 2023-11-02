50b0a3a805f50b9c6827a80eb4780b0929f59c60
Sébastien Candau <sebastiencandau>
2023-11-02 14:28:00 +0100

    reactive linterf and fix problems

--- front/src/context/Socket.tsx
+++ front/src/context/Socket.tsx
@@ -1,41 +1,41 @@
-import { createContext, useContext, useEffect } from 'react';
-import io from 'socket.io-client';
-import {useAuth} from "./Auth.tsx";
+import { createContext, useContext, useEffect } from 'react'
+import io from 'socket.io-client'
+import { useAuth } from './Auth.tsx'
 
-const WS_URL = import.meta.env.VITE_WS_URL;
+const WS_URL = import.meta.env.VITE_WS_URL
 interface WebSocketContextInterface {
-	socket: any;
+  socket: any
 }
 
 const WebSocketContext = createContext({
-	socket: null as any
-});
+  socket: null as any
+})
 
-const WebSocketProvider = (props:any) => {
-	const { token } = useAuth();
-	
-	const url = WS_URL + '?token=' + token;
-	const socket = io(url, { transports: ['websocket'] });
-	
-	useEffect(() => {
-		return () => {
-			socket.disconnect();
-		};
-	}, [socket]);
-	
-	const value : WebSocketContextInterface = {
-		socket
-	};
-	
-	return (
-		<WebSocketContext.Provider value={value}>
-			{props.children}
-		</WebSocketContext.Provider>
-	);
-};
+const WebSocketProvider = (props: any): JSX.Element => {
+  const { token } = useAuth()
+
+  const url = WS_URL + '?token=' + token
+  const socket = io(url, { transports: ['websocket'] })
+
+  useEffect(() => {
+    return () => {
+      socket.disconnect()
+    }
+  }, [socket])
+
+  const value: WebSocketContextInterface = {
+    socket
+  }
+
+  return (
+    <WebSocketContext.Provider value={value}>
+      {props.children}
+    </WebSocketContext.Provider>
+  )
+}
 
 const useWebSocket = (): WebSocketContextInterface => {
-	return useContext(WebSocketContext);
-};
+  return useContext(WebSocketContext)
+}
 
-export { WebSocketProvider, useWebSocket };
+export { WebSocketProvider, useWebSocket }
