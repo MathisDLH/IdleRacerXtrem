839d0c2e9ca0f55a8df46ab9d2039c8b6e9a2be4
pdelagrange <pierre.delagrange@hotmail.fr>
2023-11-02 10:17:48 +0100

    Add /api prefix

--- back/src/main.ts
+++ back/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
 
   app.enableCors();
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('IdleRacer API description')
     .setDescription('IdleRacer API description')
