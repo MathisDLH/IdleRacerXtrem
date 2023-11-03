99c7ed014678059f56b0e8d455ee7a1270c7b8e7
augcolas <augus.colas@orange.fr>
2023-11-03 09:24:15 +0100

    core : indent

--- front/src/components/UpgradesList.tsx
+++ front/src/components/UpgradesList.tsx
@@ -5,18 +5,18 @@ import { type UpgradeModel } from '../models/Upgrade.tsx'
 
 export default function UpgradesList (): JSX.Element {
   return (
-        <div id={'list'} className={'upgrades'}>
-            {upgrades.money.click.map((upgrade: UpgradeModel) => {
-              return (
-                    <Upgrade
-                        key={upgrade.id}
-                        id={upgrade.id}
-                        name={upgrade.name}
-                        price={upgrade.price}
-                        value={upgrade.value}
-                    />
-              )
-            })}
-        </div>
+    <div id={'list'} className={'upgrades'}>
+      {upgrades.money.click.map((upgrade: UpgradeModel) => {
+        return (
+          <Upgrade
+            key={upgrade.id}
+            id={upgrade.id}
+            name={upgrade.name}
+            price={upgrade.price}
+            value={upgrade.value}
+          />
+        )
+      })}
+    </div>
   )
 }
