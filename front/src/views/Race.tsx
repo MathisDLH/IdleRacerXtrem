ba8f121119f14a4931e334665dd01c787b38db76
Sébastien Candau <sebastiencandau>
2023-11-03 10:11:01 +0100

    imports of cars

--- front/src/views/Race.tsx
+++ front/src/views/Race.tsx
@@ -1,5 +1,15 @@
+import Car from '../components/Car'
+
 const Race = (): JSX.Element => {
-  return <h1>Race</h1>
+  return (
+    <div>
+      <Car
+      id="carRace"
+      playerName="Test"
+      skin_id={10}
+      ></Car>
+    </div>
+  )
 }
 
 export default Race
