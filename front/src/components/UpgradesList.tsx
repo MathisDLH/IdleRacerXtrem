c952e629e67081d51b9c5d0ca79bb38615c129d9
pdelagrange <pierre.delagrange@hotmail.fr>
2023-11-09 19:10:23 +0100

    change log for debug on dev

--- front/src/components/UpgradesList.tsx
+++ front/src/components/UpgradesList.tsx
@@ -79,15 +79,15 @@ export default function UpgradesList (): JSX.Element {
    * Get skins
    */
   useEffect(() => {
+    console.log('allcars', cars)
     cars.map((car): void => {
       const id = parseInt(car.split('.png')[0].split('cars/')[1]) - 1
-      console.log('SKINSS', skins);
+      console.log('id',id)
       const availables = skins
       availables.push({
         id: id,
         price: id * 100
       })
-      console.log('AVAILABLE', availables);
       setSkins(availables)
     })
   }, [])
