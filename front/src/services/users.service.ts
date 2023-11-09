166f215ce3e8d77383aa7c87382029c7a6dcdfb1
Sébastien Candau <sebastiencandau>
2023-11-09 13:50:14 +0100

    reparation score users -> user

--- front/src/services/users.service.ts
+++ front/src/services/users.service.ts
@@ -4,7 +4,7 @@ import { type User } from '../interfaces/user.interface'
 const API_BASE_URL: any = import.meta.env.VITE_API_URL
 
 export const getScores = async (): Promise<User[]> => {
-  const scores: any = await axios.get(`${API_BASE_URL}/users/scores`)
+  const scores: any = await axios.get(`${API_BASE_URL}/user/scores`)
 
   const score: User[] = scores.data
 
