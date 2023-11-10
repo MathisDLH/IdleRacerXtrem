d2e4c896e8a56dee8577d5d0d0392ec8f8d7c7ff
Mathis <mathislag@hotmail.fr>
2023-11-10 09:02:49 +0100

    modification condition prix

--- back/src/upgrade/upgrade.service.ts
+++ back/src/upgrade/upgrade.service.ts
@@ -36,15 +36,24 @@ export class UpgradeService {
   }
 
 
+  // Acheter une mise à niveau
   async buyUpgrade(buyUpgradeDto: BuyUpgradeDto, userId: number) {
+    // Log de l'appel de la fonction
     this.logger.log("CALL buyUpgrade");
+    // Récupération de la mise à niveau de l'utilisateur
     let userUpgrade = await this.redisService.getUpgrade(Number(userId), Number(buyUpgradeDto.upgradeId));
+    // Si l'utilisateur n'a pas encore cette mise à niveau
     if (Object.keys(userUpgrade).length === 0) {
+      // Récupération de la mise à niveau
       let upgrade = await this.upgradeRepository.findOne({ where: { id: Number(buyUpgradeDto.upgradeId) } });
+      // Si l'utilisateur peut créer cette mise à niveau
       if (this.canCreateUpgrade(userId, upgrade)) {
+        // Calcul du prix
         let value = upgrade.price * +buyUpgradeDto.quantity;
         let unit = upgrade.price_unit
+        // Si l'utilisateur peut payer
         if (await this.redisService.pay(userId, { value, unit })) {
+          // Ajout de la mise à niveau à l'utilisateur
           await this.redisService.addUpgrade(userId, {
             id: upgrade.id,
             amount: +buyUpgradeDto.quantity,
@@ -56,27 +65,35 @@ export class UpgradeService {
         }
       }
 
+    // Si l'utilisateur a déjà cette mise à niveau
     } else {
+      // Récupération de la mise à niveau
       let upgrade = await this.upgradeRepository.findOne({ where: { id: Number(buyUpgradeDto.upgradeId) } });
       let unit = upgrade.price_unit
 
-      if (userUpgrade.amountBought > 100000000) {
+      // Si l'utilisateur a acheté plus de 10 de cette mise à niveau
+      if (userUpgrade.amountBought >= 10) {
+        // Augmentation de l'unité de prix
         unit += Math.floor(userUpgrade.amountBought / 10) * 3;
       }
 
+      // Calcul du prix
       let value = upgrade.price * +buyUpgradeDto.quantity;
+      // Si l'utilisateur peut payer
       if (await this.redisService.pay(userId, { value, unit })) {
+        // Augmentation de la quantité de la mise à niveau
         this.redisService.incrUpgrade(userId, upgrade.id, +buyUpgradeDto.quantity, Unit.UNIT);
+        // Augmentation de la quantité achetée de la mise à niveau
         this.redisService.incrUpgradeAmountBought(userId, upgrade.id, +buyUpgradeDto.quantity)
       }
     }
+    // Log de la fin de la fonction
     this.logger.log("OK buyUpgrade");
   }
 
   async canCreateUpgrade(userId: number, upgrade: Upgrade): Promise<boolean> {
     let pastUpgrade = await this.redisService.getUpgrade(userId, upgrade.id - 1);
     if (Object.keys(pastUpgrade).length !== 0) {
-
       return true;
     }
     return false;
